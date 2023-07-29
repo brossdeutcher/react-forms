8f5932918a88ddd5b7265a715488c27686dc6f5b
Blake Rossdeutcher <blakerossdeutcher@gmail.com>
2023-07-29 10:46:25 -0600

    added token authentication

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,9 +1,38 @@
+import { useState } from "react";
 
 
 const Authenticate = ({token}) => {
+
+  const [successMsg, setSuccessMsg] = useState(null);
+  const [error, setError] = useState(null);
+
+  const authToken = async () => {
+    console.log(token);
+    
+    try {
+      const response = await fetch(`https://fsa-jwt-practice.herokuapp.com/authenticate`, {
+        method: `GET`,
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}` 
+        }
+      });
+      throw Error(`OH SHIIIIT`);
+      const result = await response.json();
+      console.log(result);
+      setSuccessMsg(result.message);
+  } catch (err) {
+    console.error(`My Error Msg: ${err}`);
+    setError(err);
+  }
+  }
+
   return (
     <>
       <h2>Authenticate</h2>
+      {successMsg && <p>Great Success!! {successMsg}</p>}
+      {error && <p>ERROR: {error}</p>}
+      <button onClick={authToken}>Authenticate Token</button>
     </>
   )
 }
